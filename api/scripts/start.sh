59edb02f986f10b80885a89b3ff6e724f04878d4
Jared Greene <jared@opentrons.com>
2017-07-18 15:29:40 -0400

    debugging BOOT

--- api/scripts/start.sh
+++ api/scripts/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "starting start scripts"
-
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 export PORTAL_SSID=Opentrons_Robot
 
@@ -10,6 +8,5 @@ echo "Exported env variables"
 echo "$(whoami)"
 /usr/local/bin/python /usr/src/api/scripts/startAP.py &
 /usr/local/bin/python /usr/src/api/scripts/startServer.py &
-echo "AFTER PYTHON"
 
 return 0
