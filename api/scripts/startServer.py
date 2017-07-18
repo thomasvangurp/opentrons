9f72e95574414701ead8f2f4d5ee9625e81aa410
Jared Greene <jared@opentrons.com>
2017-07-18 17:24:24 -0400

    new wifi indicator logo

--- api/scripts/startServer.py
+++ api/scripts/startServer.py
@@ -4,6 +4,6 @@ DEFAULT_PORT = '/dev/ttyACM0'
 
 if __name__ == '__main__':
     print("[BOOT] Server node setup")
-    robot.connect(DEFAULT_PART)
+    robot.connect(DEFAULT_PORT)
     server.start('0.0.0.0')
     print("[BOOT] Server node terminated")
