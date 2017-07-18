addad429d077e923ad9b93aba25eec8bc322e3eb
Jared Greene <jared@opentrons.com>
2017-07-18 14:52:08 -0400

    debugging dockerfile

--- api/scripts/start.sh
+++ api/scripts/start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "starting start scripts"
+
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 export PORTAL_SSID=Opentrons_Robot
+
+
+echo "Exported env variables"
 
 python /usr/src/api/scripts/startAP.py &
 python /usr/src/api/scripts/startServer.py &
