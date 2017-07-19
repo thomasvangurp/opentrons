874996fb406ec893ed84762a010ad8c1ed5acfd4
Jared Greene <jared@opentrons.com>
2017-07-19 19:11:36 -0400

    working on light patterns dockerfile

--- api/scripts/compute/start.sh
+++ api/scripts/compute/start.sh
@@ -4,8 +4,7 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 export PORTAL_SSID=Opentrons_Robot
 
 
-echo "Exported env variables"
-echo "$(whoami)"
-python /usr/src/api/scripts/startAP.py &
-python /usr/src/api/scripts/startServer.py
+echo "[BOOT] Exported env variables"
+python /usr/src/api/scripts/compute/startAP.py &
+python /usr/src/api/scripts/compute/startServer.py
 
