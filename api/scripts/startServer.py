cb847f6eedfc5399941fe5f458fbff21ddb9fadc
Jared Greene <jared@opentrons.com>
2017-07-19 10:52:28 -0400

    attempt second smoothie connect on failure during boot

--- api/scripts/startServer.py
+++ api/scripts/startServer.py
@@ -4,6 +4,12 @@ DEFAULT_PORT = '/dev/ttyACM0'
 
 if __name__ == '__main__':
     print("[BOOT] Server node setup")
-    robot.connect(DEFAULT_PORT)
+    try:
+        robot.connect(DEFAULT_PORT)
+    except RuntimeError:
+        # Sometimes it fails on the first connection attempt - not sure why.
+        # TODO: Change this hackey fix
+        print("[BOOT] Second smoothie connect attempt")
+        robot.connect(DEFAULT_PORT)
     server.start('0.0.0.0')
     print("[BOOT] Server node terminated")
