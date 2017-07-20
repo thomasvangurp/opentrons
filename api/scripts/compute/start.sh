87c994838d9fd1854a0db8850f7d1c8a00201465
Jared Greene <jared@opentrons.com>
2017-07-20 15:23:36 -0400

    fine-tuning boot process

--- api/scripts/compute/start.sh
+++ api/scripts/compute/start.sh
@@ -8,7 +8,7 @@ echo "[BOOT] Exported env variables"
 echo "[BOOT] Starting status light daemon"
 python /usr/src/api/scripts/compute/status_light.py &
 
-while [ ! -f /tmp/resin/status_light ]; do sleep 1; done
+while [ ! -p /tmp/resin/status_light ]; do sleep 1; done
 echo "[BOOT] ipc pipe created - /tmp/resin/status_light"
 
 
