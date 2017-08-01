cda88c63574c36287c1c1627dd96f8e0305de549
simplyahmazing <ahmad19526@gmail.com>
2017-08-01 16:09:18 -0400

    save notes

--- api/tests/opentrons/labware/test_liquid_tracking.py
+++ api/tests/opentrons/labware/test_liquid_tracking.py
@@ -36,3 +36,30 @@ def test_liquid_tracking():
     assert plate['A2'].volume == 200
     assert plate['A1'].volume == 4800
 
+
+def test_lt2():
+    from opentrons.tracker import Tracker
+
+
+    trash = containers_load(robot, 'point', 'A1')
+    tiprack1 = containers_load(robot, 'tiprack-10ul', 'B2')
+    plate = containers_load(robot, '96-flat', 'A2')
+
+    tracker = Tracker({well: {'red': 5, 'green': 5, 'blue': 5} for well in plate.wells()})
+
+    p200 = pipette.Pipette(
+        robot,
+        axis="b",
+        trash_container=trash,
+        tip_racks=[tiprack1],
+        max_volume=200,
+        min_volume=10,  # These are variable
+        channels=1,
+        name='other-pipette-for-transfer-tests'
+    )
+
+    p200.aspirate(plate['A1'])
+    p200.dispense(plate['A2'])
+
+    
+
